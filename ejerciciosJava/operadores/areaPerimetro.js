417a3e460f825fe79f607e07838723d8cc7cb389
3155130301 <161087654+3155130301@users.noreply.github.com>
2024-03-01 17:03:40 -0500

    Add files via upload

---
+++ ejerciciosJava/operadores/areaPerimetro.js
@@ -0,0 +1,9 @@
+console.log("5. calcular el area y perimetro del cuadrado:");
+console.log("ingrese cual es el cateto del cuadrado:");
+let cateto = 30;
+console.log (cateto);
+
+// procecso
+let  resultado_ = (cateto * cateto);
+ 
+
---
+++ ejerciciosJava/operadores/elevadoalcuadrado.js
@@ -0,0 +1,12 @@
+// entradas
+
+console.log("3.visualizar la potencia del cuadrado:");
+console.log("ingrese el numero:");
+let numero1 = 10;
+console.log (numero1);
+
+// proceso
+let resultado = (numero1 * numero1) ;
+
+// salida
+console.log (resultado);
