41700a016a3ac8edc3c05a3b6e797cc6acb6714b
3155130301 <161087654+3155130301@users.noreply.github.com>
2024-03-01 17:04:22 -0500

    Delete ejerciciosJava directory

--- ejerciciosJava/operadores/areaPerimetro.js
+++
@@ -1,9 +0,0 @@
-console.log("5. calcular el area y perimetro del cuadrado:");
-console.log("ingrese cual es el cateto del cuadrado:");
-let cateto = 30;
-console.log (cateto);
-
-// procecso
-let  resultado_ = (cateto * cateto);
- 
-
